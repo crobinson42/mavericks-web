31133a6a5527d2f9f218de21474c3b3cafdf0f4e
Cory Robinson <coryrobinson42@gmail.com>
2018-03-27 13:18:10 -0700

    chore(): add release version to Sentry

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+
 import App from './App'
 import AppErrorBoundery from './AppErrorBoundery'
 import registerServiceWorker from './registerServiceWorker'
 
+import pckg from '../package.json'
+
+import './index.css'
+
+
 import swal from 'sweetalert'
+
+window.Raven.config('https://f12d35a63e3f4264a168eb0e8caa6802@sentry.io/287059', {
+  release: pckg.version,
+}).install()
 
 ReactDOM.render(
   <AppErrorBoundery>
